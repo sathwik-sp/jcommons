6d1b08d21c4c1342e57a892c3f0fa7b455f5dcbe
Aditya Pawade <aditya.pawade@flipkart.com>
2022-03-24 17:15:56 +0530

    added equals in arithmetic utils

--- src/main/java/com/adtsw/jcommons/utils/ArithmeticUtil.java
+++ src/main/java/com/adtsw/jcommons/utils/ArithmeticUtil.java
@@ -2,8 +2,9 @@ package com.adtsw.jcommons.utils;
 
 public class ArithmeticUtil {
 
-    public static <V extends Number> V add(V lhs, V rhs)
-    {
+    private static final double EPSILON = 0.000000000000001d;
+
+    public static <V extends Number> V add(V lhs, V rhs) {
         if (lhs.getClass() == Integer.class && rhs.getClass() == Integer.class)
             return (V) Integer.valueOf(((Integer) lhs) + ((Integer) rhs));
         if (lhs.getClass() == Double.class && rhs.getClass() == Double.class)
@@ -15,8 +16,7 @@ public class ArithmeticUtil {
         throw new IllegalArgumentException("unsupported types: " + lhs.getClass() + " & " + rhs.getClass());
     }
 
-    public static <V extends Number> V subtract(V lhs, V rhs)
-    {
+    public static <V extends Number> V subtract(V lhs, V rhs) {
         if (lhs.getClass() == Integer.class && rhs.getClass() == Integer.class)
             return (V) Integer.valueOf(((Integer) lhs) - ((Integer) rhs));
         if (lhs.getClass() == Double.class && rhs.getClass() == Double.class)
@@ -28,8 +28,7 @@ public class ArithmeticUtil {
         throw new IllegalArgumentException("unsupported types: " + lhs.getClass() + " & " + rhs.getClass());
     }
 
-    public static <V extends Number> V multiply(V lhs, V rhs)
-    {
+    public static <V extends Number> V multiply(V lhs, V rhs) {
         if (lhs.getClass() == Integer.class && rhs.getClass() == Integer.class)
             return (V) Integer.valueOf(((Integer) lhs) * ((Integer) rhs));
         if (lhs.getClass() == Integer.class && rhs.getClass() == Double.class)
@@ -45,8 +44,7 @@ public class ArithmeticUtil {
         throw new IllegalArgumentException("unsupported types: " + lhs.getClass() + " & " + rhs.getClass());
     }
 
-    public static <V extends Number> V divide(V lhs, V rhs)
-    {
+    public static <V extends Number> V divide(V lhs, V rhs) {
         if (lhs.getClass() == Integer.class && rhs.getClass() == Integer.class)
             return (V) Integer.valueOf(((Integer) lhs) / ((Integer) rhs));
         if (lhs.getClass() == Double.class && rhs.getClass() == Double.class)
@@ -58,21 +56,48 @@ public class ArithmeticUtil {
         throw new IllegalArgumentException("unsupported types: " + lhs.getClass() + " & " + rhs.getClass());
     }
 
-    public static <V extends Number> V mod(V lhs, V rhs)
-    {
+    public static <V extends Number> V mod(V lhs, V rhs) {
+
         if (lhs.getClass() == Integer.class && rhs.getClass() == Integer.class)
             return (V) Integer.valueOf(((Integer) lhs) % ((Integer) rhs));
+        if (lhs.getClass() == Double.class && rhs.getClass() == Integer.class)
+            return (V) Double.valueOf(((Double) lhs) % ((Integer) rhs));
+        if (lhs.getClass() == Float.class && rhs.getClass() == Integer.class)
+            return (V) Float.valueOf(((Float) lhs) % ((Integer) rhs));
+        if (lhs.getClass() == Long.class && rhs.getClass() == Integer.class)
+            return (V) Long.valueOf(((Long) lhs) % ((Integer) rhs));
+
+        if (lhs.getClass() == Integer.class && rhs.getClass() == Double.class)
+            return (V) Double.valueOf(((Integer) lhs) % ((Double) rhs));
         if (lhs.getClass() == Double.class && rhs.getClass() == Double.class)
             return (V) Double.valueOf(((Double) lhs) % ((Double) rhs));
+        if (lhs.getClass() == Float.class && rhs.getClass() == Double.class)
+            return (V) Double.valueOf(((Float) lhs) % ((Double) rhs));
+        if (lhs.getClass() == Long.class && rhs.getClass() == Double.class)
+            return (V) Double.valueOf(((Long) lhs) % ((Double) rhs));
+
+        if (lhs.getClass() == Integer.class && rhs.getClass() == Float.class)
+            return (V) Float.valueOf(((Integer) lhs) % ((Float) rhs));
+        if (lhs.getClass() == Double.class && rhs.getClass() == Float.class)
+            return (V) Double.valueOf(((Double) lhs) % ((Float) rhs));
         if (lhs.getClass() == Float.class && rhs.getClass() == Float.class)
             return (V) Float.valueOf(((Float) lhs) % ((Float) rhs));
+        if (lhs.getClass() == Long.class && rhs.getClass() == Float.class)
+            return (V) Float.valueOf(((Long) lhs) % ((Float) rhs));
+
+        if (lhs.getClass() == Integer.class && rhs.getClass() == Long.class)
+            return (V) Long.valueOf(((Integer) lhs) % ((Long) rhs));
+        if (lhs.getClass() == Double.class && rhs.getClass() == Long.class)
+            return (V) Double.valueOf(((Double) lhs) % ((Long) rhs));
+        if (lhs.getClass() == Float.class && rhs.getClass() == Long.class)
+            return (V) Float.valueOf(((Float) lhs) % ((Long) rhs));
         if (lhs.getClass() == Long.class && rhs.getClass() == Long.class)
             return (V) Long.valueOf(((Long) lhs) % ((Long) rhs));
+
         throw new IllegalArgumentException("unsupported types: " + lhs.getClass() + " & " + rhs.getClass());
     }
 
-    public static <V extends Number> Double pow(V lhs, V rhs)
-    {
+    public static <V extends Number> Double pow(V lhs, V rhs) {
         if (lhs.getClass() == Integer.class && rhs.getClass() == Integer.class)
             return Math.pow(Double.valueOf((Integer) lhs), Double.valueOf((Integer) rhs));
         if (lhs.getClass() == Double.class && rhs.getClass() == Double.class)
@@ -84,8 +109,7 @@ public class ArithmeticUtil {
         throw new IllegalArgumentException("unsupported types: " + lhs.getClass() + " & " + rhs.getClass());
     }
 
-    public static <V extends Number> boolean lessThan(V lhs, V rhs)
-    {
+    public static <V extends Number> boolean lessThan(V lhs, V rhs) {
         if (lhs.getClass() == Integer.class && rhs.getClass() == Integer.class)
             return ((Integer) lhs) < ((Integer) rhs);
         if (lhs.getClass() == Double.class && rhs.getClass() == Double.class)
@@ -97,8 +121,7 @@ public class ArithmeticUtil {
         throw new IllegalArgumentException("unsupported types: " + lhs.getClass() + " & " + rhs.getClass());
     }
 
-    public static <V extends Number> boolean lessThanOrEqualTo(V lhs, V rhs)
-    {
+    public static <V extends Number> boolean lessThanOrEqualTo(V lhs, V rhs) {
         if (lhs.getClass() == Integer.class && rhs.getClass() == Integer.class)
             return ((Integer) lhs) <= ((Integer) rhs);
         if (lhs.getClass() == Double.class && rhs.getClass() == Double.class)
@@ -110,8 +133,7 @@ public class ArithmeticUtil {
         throw new IllegalArgumentException("unsupported types: " + lhs.getClass() + " & " + rhs.getClass());
     }
 
-    public static <V extends Number> boolean moreThan(V lhs, V rhs)
-    {
+    public static <V extends Number> boolean moreThan(V lhs, V rhs) {
         if (lhs.getClass() == Integer.class && rhs.getClass() == Integer.class)
             return ((Integer) lhs) > ((Integer) rhs);
         if (lhs.getClass() == Double.class && rhs.getClass() == Double.class)
@@ -123,8 +145,7 @@ public class ArithmeticUtil {
         throw new IllegalArgumentException("unsupported types: " + lhs.getClass() + " & " + rhs.getClass());
     }
 
-    public static <V extends Number> boolean moreThanOrEqualTo(V lhs, V rhs)
-    {
+    public static <V extends Number> boolean moreThanOrEqualTo(V lhs, V rhs) {
         if (lhs.getClass() == Integer.class && rhs.getClass() == Integer.class)
             return ((Integer) lhs) >= ((Integer) rhs);
         if (lhs.getClass() == Double.class && rhs.getClass() == Double.class)
@@ -134,5 +155,15 @@ public class ArithmeticUtil {
         if (lhs.getClass() == Long.class && rhs.getClass() == Long.class)
             return ((Long) lhs) >= ((Long) rhs);
         throw new IllegalArgumentException("unsupported types: " + lhs.getClass() + " & " + rhs.getClass());
+    }
+
+    public static <V extends Number> boolean equalTo(V lhs, V rhs) {
+        if (lhs == null) {
+            return lhs == null;
+        } else if (rhs == null) {
+            return false;
+        } else {
+            return Math.abs(lhs.doubleValue() - rhs.doubleValue()) < EPSILON;
+        }
     }
 }
